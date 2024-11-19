e5a0986a02ea53faff7cd83330470a9e0620f4ec
David Tolmay <davidt@missionreadyhq.com>
2024-11-19 19:18:30 +1300

    setup localhost conection to azure db and have a working endpoint

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mysql = require("mysql2");
+const cors = require("cors");
+const fs = require("fs");
 require("dotenv").config();
 
 const app = express();
@@ -14,9 +16,15 @@ const pool = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASS,
   database: process.env.MYSQL_DATABASE,
+  port: process.env.PORT,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync("./DigiCertGlobalRootCA.crt.pem"),
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  connectTimeout: 20000, // Optional: Set a higher timeout
 });
 
 // Default route handler
@@ -24,8 +32,19 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.get("/insurance", (req, res) => {
+  console.log("GET /insurance endpoint was hit 🎯");
+  pool.query("SELECT * FROM insurance_quote", (err, results) => {
+    if (err) {
+      console.error("Error fetching Insurance:", err);
+      return res.status(500).send("Internal Server Error");
+    }
+    res.json(results);
+  });
+});
+
 // set up port
-const PORT = process.env.PORT;
+const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}`);
 });
